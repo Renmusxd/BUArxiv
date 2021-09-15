90bc9dd80f867ee47f5bb5c9a335f31ae18dcc40
Sumner Hearth <sumnernh@gmail.com>
2021-09-15 18:42:06 -0400

    Use unstructured data to update specific fields

--- scrape.py
+++ scrape.py
@@ -6,6 +6,11 @@ from sqlalchemy_utils import database_exists
 from sqlalchemy.orm import Session
 from website.database import ArxivEntry
 import re
+import json
+import logging
+
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.ERROR)
 
 
 def get_authors_and_tags(dir):
@@ -21,7 +26,53 @@ def get_authors_and_tags(dir):
 
 
 def run_scrape(authors, author_tags, db_file, strip_versions=True):
-    engine = create_engine(db_file, echo=True, future=True)
+    def update_url(res, instance):
+        if res.doi:
+            url = doi_prefix + res.doi
+        else:
+            url = arxiv_prefix + res_id
+        if url != instance.url:
+            instance.url = url
+    def update_timestamp(res, instance):
+        if res.published != instance.timestamp:
+            instance.timestamp = res.published
+    def update_title(res, instance):
+        if res.title != instance.title:
+            instance.title = res.title
+    def update_abstact(res, instance):
+        abstract = res.summary.replace('\n', ' ')
+        if abstract != instance.abstract:
+            instance.abstract = abstract
+    def update_authors(res, instance):
+        authors = ", ".join((author.name for author in res.authors))
+        if instance.authors != authors:
+            instance.authors = authors
+    def update_journal(res, instance):
+        if instance.journal_ref != res.journal_ref:
+            instance.journal_ref = res.journal_ref
+    def update_doi(res, instance):
+        if res.doi != instance.doi:
+            instance.doi = res.doi
+    def update_tags(res, instance):
+        tags = ", ".join(res.categories)
+        if tags != instance.tags:
+            instance.tags = tags
+    def update_unstructured(res, instance):
+        instance.unstructured = '{"autoupdates": ["doi", "journal"]}'
+
+    updaters = {
+        'url': update_url,
+        'timestamp': update_timestamp,
+        'title': update_title,
+        'abstract': update_abstact,
+        'authors': update_authors,
+        'journal': update_journal,
+        'doi': update_doi,
+        'tags': update_tags,
+        'unstructured': update_unstructured
+    }
+
+    engine = create_engine(db_file, echo=False, future=True)
     if not database_exists(engine.url):
         ArxivEntry.metadata.create_all(engine)
 
@@ -60,22 +111,25 @@ def run_scrape(authors, author_tags, db_file, strip_versions=True):
                                    image_url=None,
                                    tags=", ".join(res.categories),
                                    autoupdate=True,
-                                   hidden=False)
+                                   hidden=False,
+                                   unstructured_data='{"autoupdates": ["doi", "journal"]}')
                     session.add(a)
-                elif instance.autoupdate:
-                    if res.doi:
-                        url = doi_prefix + res.doi
-                    else:
-                        url = arxiv_prefix + res_id
-                    instance.url = url
-                    instance.timestamp = res.published
-                    instance.title = res.title
-                    instance.abstract = res.summary.replace('\n', ' ')
-                    instance.authors = ", ".join((author.name for author in res.authors))
-                    instance.timestamp = res.published
-                    instance.journal_ref = res.journal_ref
-                    instance.doi = res.doi
-                    instance.tags = ", ".join(res.categories)
+                else:
+                    autoupdates = []
+                    if instance.autoupdate:
+                        autoupdates = updaters.keys()
+                    elif instance.unstructured:
+                        try:
+                            unstructured_data = json.loads(instance.unstructured)
+                            if 'autoupdates' in unstructured_data:
+                                autoupdates = list(unstructured_data['autoupdates'])
+                        except json.decoder.JSONDecodeError:
+                            pass
+                        except Exception as e:
+                            print("Unknown exception: {}".format(e))
+                    for updater in autoupdates:
+                        if updater in updaters:
+                            updaters[updater](res, instance)
             session.commit()
 
 
